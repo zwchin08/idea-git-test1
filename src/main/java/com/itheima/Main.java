9605dca4b9df877b3d7ed958e72cfeb94dd9aa32
chenzhengwei <zwchin08@gmail.com>
2024-05-12 13:10:10 +0900

    第一次测试

---
+++ src/main/java/com/itheima/Main.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.itheima
+ * Description:
+ *
+ * @Author 吉田学園　陈正伟
+ * @Create 2024-05-12 13:08
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
