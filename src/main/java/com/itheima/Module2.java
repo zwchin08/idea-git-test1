69ae028164c43af866ceee87fb2f3b8438406b3c
chenzhengwei <zwchin08@gmail.com>
2024-05-12 13:52:33 +0900

    第2次测试

---
+++ src/main/java/com/itheima/Module2.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * ClassName: Module2
+ * Package: com.itheima
+ * Description:
+ *
+ * @Author 吉田学園　陈正伟
+ * @Create 2024-05-12 13:49
+ * @Version 1.0
+ */
+public class Module2 {
+    public static void main(String[] args) {
+        System.out.println("完成模块2的开发");
+        System.out.println("优化了模块2 的开发");
+    }
+}
